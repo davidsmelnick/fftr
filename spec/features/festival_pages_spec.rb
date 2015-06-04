64113ca354e069225926aeee3689643ab012a311
Alex Williamson <alexilliamson@gmail.com>
2015-06-04 10:53:11 -0700

    Root Test

--- spec/features/festival_pages_spec.rb
+++ spec/features/festival_pages_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 feature "Festival Pages" do
 
-    visit root_path
+  scenario do
+    before{visit root_path}
 
     expect(page).to have_title('Festivals for the Restivals')
+  end
 
 
 end
