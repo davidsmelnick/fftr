806cceb0a1dcf7cf1df16f47ca752541b9a02064
David Melnick <davidsmelnick@gmail.com>
2015-06-08 15:36:56 +0000

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605165215) do
+ActiveRecord::Schema.define(version: 20150606124717) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "festivals", force: :cascade do |t|
     t.string   "name"
